377cdf299380b5e73bb89d5077419e817447bad2
Khoubaib Boughalmi <khoubaib.kb99@gmail.com>
2023-11-26 20:45:14 +0100

    added logout functionality

--- frontend/src/components/miscellaneous/SideDrawer.jsx
+++ frontend/src/components/miscellaneous/SideDrawer.jsx
@@ -3,18 +3,18 @@ import { useDisclosure } from "@chakra-ui/hooks";
 import { Input } from "@chakra-ui/input";
 import { Box, Text } from "@chakra-ui/layout";
 import {
-  Menu,
-  MenuButton,
-  MenuDivider,
-  MenuItem,
-  MenuList,
+	Menu,
+	MenuButton,
+	MenuDivider,
+	MenuItem,
+	MenuList,
 } from "@chakra-ui/menu";
 import {
-  Drawer,
-  DrawerBody,
-  DrawerContent,
-  DrawerHeader,
-  DrawerOverlay,
+	Drawer,
+	DrawerBody,
+	DrawerContent,
+	DrawerHeader,
+	DrawerOverlay,
 } from "@chakra-ui/modal";
 import { Tooltip } from "@chakra-ui/tooltip";
 import { BellIcon, ChevronDownIcon } from "@chakra-ui/icons";
@@ -27,68 +27,74 @@ import { useChatContext } from "../../context/chatProvider";
 import ProfileModal from "../miscellaneous/profileModel"
 
 function SideDrawer() {
-  const { user } = useChatContext();
-  
-  const [search, setSearch] = useState("");
-  const [searchResult, setSearchResult] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [loadingChat, setLoadingChat] = useState(false);
+	const { user } = useChatContext();
+	const navigate = useNavigate();
 
-  const toast = useToast();
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  
-  return (
-    <>
-      <Box
-        display="flex"
-        justifyContent="space-between"
-        alignItems="center"
-        bg="white"
-        w="100%"
-        p="5px 10px 5px 10px"
-        borderWidth="5px"
-      >
-        <Tooltip label="Search Users to chat" hasArrow placement="bottom-end">
-          <Button variant="ghost" onClick={onOpen}>
-            <i className="fas fa-search"></i>
-            <Text d={{ base: "none", md: "flex" }} px={4}>
-              Search User
-            </Text>
-          </Button>
-        </Tooltip>
-        <Text fontSize="2xl" fontFamily="Work sans">
-          Talk-A-Tive
-        </Text>
-        <div>
-            <Menu>
-                <MenuButton p={1}>
-                
-                <BellIcon fontSize="2xl" m={1} />
-                </MenuButton>
-                {/*<MenuList pl={2}></MenuList>*/}
-            </Menu>
-            <Menu>
-            <MenuButton as={Button} bg="white" rightIcon={<ChevronDownIcon />}>
-              <Avatar
-                size="sm"
-                cursor="pointer"
-                name={user.name}
-                src={user.avatar}
-              />
-            </MenuButton>
-            <MenuList>
+	const [search, setSearch] = useState("");
+	const [searchResult, setSearchResult] = useState([]);
+	const [loading, setLoading] = useState(false);
+	const [loadingChat, setLoadingChat] = useState(false);
 
-            <ProfileModal user={user}>
-                <MenuItem>My Profile</MenuItem>{" "}
-            </ProfileModal>
-            <MenuDivider />
-            <MenuItem onClick={()=>alert("")}>Logout</MenuItem>
-            </MenuList>
-          </Menu>
-        </div>
-      </Box>
-    </>
-  );
+	const toast = useToast();
+	const { isOpen, onOpen, onClose } = useDisclosure();
+
+	const handleLogoutFn = () => {
+		localStorage.removeItem("userInfo");
+		navigate("/");
+	}
+
+	return (
+		<>
+			<Box
+				display="flex"
+				justifyContent="space-between"
+				alignItems="center"
+				bg="white"
+				w="100%"
+				p="5px 10px 5px 10px"
+				borderWidth="5px"
+			>
+				<Tooltip label="Search Users to chat" hasArrow placement="bottom-end">
+				<Button variant="ghost" onClick={onOpen}>
+					<i className="fas fa-search"></i>
+					<Text d={{ base: "none", md: "flex" }} px={4}>
+					Search User
+					</Text>
+				</Button>
+				</Tooltip>
+				<Text fontSize="2xl" fontFamily="Work sans">
+				Talk-A-Tive
+				</Text>
+				<div>
+					<Menu>
+						<MenuButton p={1}>
+						
+						<BellIcon fontSize="2xl" m={1} />
+						</MenuButton>
+						{/*<MenuList pl={2}></MenuList>*/}
+					</Menu>
+					<Menu>
+					<MenuButton as={Button} bg="white" rightIcon={<ChevronDownIcon />}>
+					<Avatar
+						size="sm"
+						cursor="pointer"
+						name={user.name}
+						src={user.avatar}
+					/>
+					</MenuButton>
+					<MenuList>
+
+					<ProfileModal user={user}>
+						<MenuItem>My Profile</MenuItem>{" "}
+					</ProfileModal>
+					<MenuDivider />
+					<MenuItem onClick={handleLogoutFn}>Logout</MenuItem>
+					</MenuList>
+				</Menu>
+				</div>
+			</Box>
+		</>
+	);
 }
 
 export default SideDrawer;
